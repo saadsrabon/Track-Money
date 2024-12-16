d0131765d5aa8a04d4c7130e35931420b08a287d
Saad Srabon <saadsrabon2@gmail.com>
2024-12-17 02:09:47 +0600

    Button toggle added

---
+++ src/components/Buttons/ExpenseIncomeBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ExpenseIncomeBtn({name ,isActive ,setIsActive,id}) {
+  return (
+    <div onClick={()=>setIsActive({
+        ...isActive,
+        id:id
+    })} className={`cursor-pointer text-center flex-1 px-4 py-2 hover:bg-slate-50 hover:text-slate-900 ${isActive?.id==id?"active":""}`}>
+          {name}
+        </div>
+  )
+}
--- src/components/InputOutput.jsx
+++ src/components/InputOutput.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import ExpenseIncomeBtn from "./Buttons/ExpenseIncomeBtn";
 
 
 export default function InputOutput() {
+    const [isActive ,setIsActive]= useState({id:"1"})
   return (
     <div className="p-6 py-8 bg-[#F9FAFB] border rounded-md">
     <h2 className="text-3xl font-semibold leading-7 text-gray-800 text-center">Expense Tracker</h2>
@@ -9,12 +12,9 @@ export default function InputOutput() {
       <div
         className="flex divide-x divide-slate-400/20 overflow-hidden rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 shadow-sm ring-1 ring-slate-700/10 mt-6"
       >
-        <div className="cursor-pointer text-center flex-1 px-4 py-2 hover:bg-slate-50 hover:text-slate-900 active">
-          Expense
-        </div>
-        <div className="cursor-pointer text-center flex-1 px-4 py-2 hover:bg-slate-50 hover:text-slate-900">
-          Income
-        </div>
+        <ExpenseIncomeBtn id="1" isActive={isActive} setIsActive={setIsActive} name="Income"/>
+        <ExpenseIncomeBtn id="2" isActive={isActive} setIsActive={setIsActive} name="Expense"/>
+        
       </div>
 
       {/* <!-- Note --> */}

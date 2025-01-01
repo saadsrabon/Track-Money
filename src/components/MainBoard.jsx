6ab9a31649ea91cf8ac2f3da4450344a6cd3a93f
Saad Srabon <saadsrabon2@gmail.com>
2025-01-01 21:58:33 +0600

    Add data tracking and calculations for income and expenses

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import InputOutput from "./InputOutput";
 import RightPart from "./RightPart";
 
 export default function MainBoard() {
+  const data = [
+    { category: "salary", ammount: 500,type:"income",date:"2021-09-01" },
+    { category:"health", ammount: 1000 ,type:"expense",date:"2021-09-01"},
+    { category:"food", ammount: 1500 ,type:"expense",date:"2021-09-01"},
+    { category:"bond", ammount: 2500 ,type:"income",date:"2021-09-01"},
+  
+
+  
+  ];
+
+
+  const [trackData, setTrackData] = useState(data);
   return (
     <main className="relative mx-auto mt-10 w-full max-w-7xl">
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -9,7 +22,7 @@ export default function MainBoard() {
 
         <InputOutput />
         {/* <!-- Right Columndd --> */}
-        <RightPart />
+        <RightPart data={trackData}  />
       </section>
     </main>
   );
--- src/components/RightPart.jsx
+++ src/components/RightPart.jsx
@@ -1,15 +1,38 @@
 import Dashboard from "./Dashboard";
 import TotalBoard from "./TotalBoard";
 
+export default function RightPart({ data }) {
+  const balance = data.reduce((acc, curr) => {
+    if (curr.type === "income") {
+      return acc + curr.ammount;
+    } else {
+      return acc - curr.ammount;
+    }
+  }, 0);
 
-export default function RightPart() {
+  const income = data.reduce((acc, curr) => {
+    if (curr.type === "income") {
+      return acc + curr.ammount;
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  const expense = data.reduce((acc, curr) => {
+    if (curr.type === "expense") {
+      return acc + curr.ammount;
+    } else {
+      return acc;
+    }
+  }, 0);
+
   return (
     <div className="lg:col-span-2">
-    {/* <!-- Total Balance Stat--> */}
-    <TotalBoard/>
+      {/* <!-- Total Balance Stat--> */}
+      <TotalBoard balance={balance} income={income} expense={expense} />
 
-    {/* <!-- List Down --> */}
-    <Dashboard/>
-  </div>
-  )
+      {/* <!-- List Down --> */}
+      <Dashboard />
+    </div>
+  );
 }
